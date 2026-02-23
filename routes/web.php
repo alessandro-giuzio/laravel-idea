32828296beab26b1b40c6b4b51e8673984002e42
alessandro-giuzio <giuzio@icloud.com>
2026-02-23 20:01:40 +0100

    Remove commented sections from routes/web.php for cleaner code

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,15 @@
 
 use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Idea;
 
 
 // index
 Route::get('/ideas', [IdeaController::class, 'index']);
-
-// create
 Route::get('/ideas/create', [IdeaController::class, 'create']);
-
-// store
 Route::post('/ideas', [IdeaController::class, 'store']);
-
-// show single idea
 Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
-
-// edit
 Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
-
-// update idea
 Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
-
-// destroy
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
 
 Route::view('/', 'index');
