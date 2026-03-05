c851e0adc3c0abc5ddfb6fc4c81fc9a1cdc9fa59
alessandro-giuzio <giuzio@icloud.com>
2026-03-05 15:33:08 +0100

    Fix navigation links in the navbar: add href attributes for Home, New Idea, and About links

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -8,8 +8,10 @@
                 </svg>
             </div>
             <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                <li><a>Home</a></li>
-                <li><a>New Idea</a></li>
+                <li><a href="/ideas">Home</a></li>
+                <li><a href="/ideas/create">New Idea</a></li>
+                <li><a href="/about">About</a>
+                </li>
             </ul>
         </div>
         <a class="btn btn-ghost text-xl">Idea</a>
@@ -18,6 +20,7 @@
         <ul class="menu menu-horizontal px-1">
             <li><a href="/ideas">Home</a></li>
             <li><a href="/ideas/create">New Idea</a></li>
+            <li><a href="/about">About</a></li>
 
         </ul>
         </details>
